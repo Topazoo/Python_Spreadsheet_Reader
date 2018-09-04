d2bfb385dc97584bc9cdc98c22791e7fe277f8a5
U-SWANSON-PETER\topaz <topazoo42@gmail.com>
2018-09-04 03:56:26 -0700

    Ready for public use

---
+++ README.md
@@ -0,0 +1,74 @@
+# Spreadsheet Reader
+### Author: Peter Swanson
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Python 2.7](https://img.shields.io/badge/Python-2.7-brightgreen.svg)](https://www.python.org/downloads/release/python-2714/)
+[![openpyxl 2.5.6](https://img.shields.io/badge/openpyxl-2.5.6-brightgreen.svg)](https://pypi.org/project/openpyxl/)
+
+## Background:
+<b>This code allows a user to write large quantities of Python data to spreadsheet rows or columns.</b>
+
+## Using the Code:
+### Installing Dependencies:
+Ensure the following are installed on the machine you are running the application on:
+- Python 2.7 with Pip
+- Virtualenv for Python 2.7
+
+Create a virtualenv and install the requirements from <i>requirements.txt</i> with Pip
+```
+$ pip install -r "requirements.txt"
+``` 
+
+### Reading Spreadsheets:
+Spreadsheets can be opened by instantiating a <i>Spreadsheet</i> object with the name of the sheet.
+The extension is optional.
+```
+>>> from Spreadsheet import Spreadsheet
+>>> sheet = Spreadsheet("test_sheet.xlsx")
+```
+Cells can be read with the <i>Spreadsheet.read_row()</i> and <i>Spreadsheet.read_column()</i>
+functions. Functions return a dictionary containing the read values. 
+Row and column indices begin at 1.
+```
+>>> print sheet.read_row(row=1, start_col=1)
+{u'Row 1': [u'Row', u'Text']}
+>>> print sheet.read_column(col=1, header=True, start_row=1)
+{u'Row': [u'Column', u'Text']}
+```
+If header=True in the <i>Spreadsheet.read_column()</i> method, The first value will be considered
+the header and used as the column's dictionary key (see above example). 
+
+### Writing and Appending to Spreadsheets:
+Open spreadsheets can be written or appended to.
+
+#### Writing to Spreadsheets:
+Spreadsheets can be written to with the <i>Spreadsheet.write_row()</i> and <i>Spreadsheet.write_column()</i>
+methods. The <i>content</i> parameter must be a list of strings. Each string in the list will be written to
+a concurrent cell.
+```
+>>> sheet.write_row(row=1, content=['These', 'Are', 'Headers'], start_col=1, bold=True, italics=False) 
+# Writes These, Are, and Headers in bold to the top three cells horizontally in bold.
+>>> sheet.write_column(col=1, content=['Here', 'Are', 'Values'], start_row=2, bold=False, italics=True) 
+# Writes Here, Are, and Values in italics to three columns under the 'These' heading.
+```
+
+#### Appending to Spreadsheets:
+Spreadsheets can be appended to with the <i>Spreadsheet.append_row()</i> and <i>Spreadsheet.append_column()</i>
+methods. The <i>content</i> parameter must be a list of strings. Each string in the list will be written to
+a concurrent cell.
+```
+>>> sheet.append_row(row=1, content=['More', 'Up', 'Top'], bold=True, italics=False) 
+# Writes More, Up, and Top in bold after 'Headers.'
+>>> sheet.append_column(col=1, content=['More', 'In', 'Col'], bold=False, italics=True) 
+# Writes More, In, and Col in italics to three columns under 'Values.'
+```
+
+#### Saving Spreadsheets:
+Spreadsheets can be saved with the <i>Spreadsheet.save()</i> method. Sheets are saved automatically 
+when they are written to.
+```
+>>> sheet.save()
+# Saves the sheet
+```
+
+## Files
+- <i>Spreadsheet.py</i> - Class for reading and writing to spreadsheets.
---
+++ Spreadsheet.py
@@ -0,0 +1,221 @@
+#!/usr/bin/python
+
+""" Author: Peter Swanson
+            pswanson@ucdavis.edu
+
+    Description: A class to make interacting with spreadsheets via 
+    the openpyxl library easier
+
+    Version: Python 2.7
+    Requirements: openpyxl """
+
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Font
+import os
+
+class Spreadsheet(object):
+    """ Class to simplify operating on Excel sheets """
+
+    def __init__(self, filename, load=True):
+        """ @filename - The name of the excel file including the extension
+            @load - Load the file if true, create it if false """
+
+        # Add extension
+        if filename[-5:-1:] != ".xlsx":
+            filename += ".xlsx"
+
+        self.filename = filename
+
+        # If load is False, recreate excel file on every run
+        # Else load the file
+        if load is True:
+            try:
+                self.file = load_workbook(filename=filename)
+            except IOError:
+                print "Failed to load " + filename + "."
+                print "Please check if the document exists and try again."
+                exit(1)
+
+        else:
+            if os.path.isfile(filename):
+                os.remove(filename)
+
+            self.file = Workbook()
+            self.save()
+        
+        # Work on default sheet
+        self.sheet = self.file.active
+
+    def save(self):
+        """ Save the file """
+
+        try:
+            self.file.save(filename=self.filename)
+        except IOError:
+            print "Failed to save " + self.filename + "."
+            print "Please check if the document is open and try again."
+            exit(1)
+
+    def write(self, content, cell):
+        """ Write to a cell
+            @content - The content to write
+            @cell - The cell to write to """
+
+        self.sheet[cell] = content
+        self.save()
+
+    def read_column(self, col, header=True, start_row=1):
+        """ Read values from a column
+            @col - The column to read
+            @header - True if the column has a header. Used as column dict key 
+            @start_row - The row to start reading the column from """
+
+        # Column stored in a dict with a single list
+        values = []
+        column = {}
+
+        row = start_row
+
+        # Header is dict key, values is a list of cell values from the column
+        if header:
+            column[self.sheet[row][col - 1].value] = values
+            row += 1
+        else:
+            column["None"] = values
+
+        # Store values
+        while True:
+            cell = self.sheet.cell(row=row, column=col).value
+
+            if cell is None:
+                break
+
+            values.append(cell)
+            row += 1
+
+        return column
+
+    def read_row(self, row, start_col=1):
+        """ Read values from a row
+            @row - The row to read
+            @start_col - The column to start reading th row from """
+
+        # Row stored in a dict with a single list
+        values = []
+        obj = {}
+        obj['Row: ' + str(row)] = values
+
+        col = start_col
+
+        # Store values
+        while True:
+            cell = self.sheet.cell(row=row, column=col).value
+
+            if cell is None:
+                break
+
+            values.append(cell)
+            col += 1
+
+        return obj
+
+    def write_column(self, col, content, start_row=1, bold=False, italics=False):
+        """ Write values to a column
+            @col - The column to write to
+            @content - A list of strings to write 
+            @start_row - The row to start writing from
+            @bold - True if text should be bolded
+            @italics - True if text should be italicized """
+
+        # Column stored in a dict with a single list
+        row = start_row
+
+        # Store values
+        for value in content:
+            try:
+                cell = self.sheet.cell(row=row, column=col, value=value.encode('utf-8'))
+            except UnicodeEncodeError:
+                cell = self.sheet.cell(row=row, column=col, value=value.decode('utf-8'))
+            except UnicodeDecodeError:
+                cell = self.sheet.cell(row=row, column=col, value="Content Error")
+
+            cell.font = Font(bold=bold, italic=italics)
+            row += 1
+
+        self.save()
+
+    def write_row(self, row, content, start_col=1, bold=False, italics=False):
+        """ Write values to a row
+            @row - The row to write to
+            @content - A list of strings to write 
+            @start_col - The column to start writing at
+            @bold - True if text should be bolded
+            @italics - True if text should be italicized """
+
+        # Column stored in a dict with a single list
+        col = start_col
+
+        # Store values
+        for value in content:
+            try:
+                cell = self.sheet.cell(row=row, column=col, value=value.encode('utf-8'))
+            except UnicodeEncodeError:
+                cell = self.sheet.cell(row=row, column=col, value=value.decode('utf-8'))
+            except UnicodeDecodeError:
+                cell = self.sheet.cell(row=row, column=col, value="Content Error")
+
+            cell.font = Font(bold=bold, italic=italics)
+            col += 1
+
+        self.save()
+
+    def append_row(self, row, content, bold=False, italics=False):
+        """ Append content to row
+            @row - The row to append to
+            @content - A list of strings to write 
+            @bold - True if text should be bolded
+            @italics - True if text should be italicized """
+
+        col = 1
+
+        # Search for next empty column in row
+        while True:
+            cell = self.sheet.cell(row=row, column=col).value
+
+            if cell is None:
+                break
+
+            col += 1
+
+        # Write after
+        self.write_row(row, content, col, bold, italics)
+
+    def append_column(self, col, content, bold=False, italics=False):
+        """ Append content to column 
+            @col - The column to append to
+            @content - A list of strings to write 
+            @bold - True if text should be bolded
+            @italics - True if text should be italicized """
+
+        row = 1
+
+        # Search for next empty row in column
+        while True:
+            cell = self.sheet.cell(row=row, column=col).value
+
+            if cell is None:
+                break
+
+            row += 1
+
+        # Write after
+        self.write_column(col, content, row, bold, italics)
+
+    def create_sheets(self, values):
+        """ Create sheets to write to
+            @values - A list of strings to title sheets """
+
+        for value in values:
+            self.file.create_sheet(value)
+
+        self.save()
